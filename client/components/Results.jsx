0ea61a210299f53ab5d3b303fcbe0a459ed20f84
EDA <eda@Mac-Mini-4.local>
2017-05-26 15:18:46 +1200

    added styling

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -7,47 +7,69 @@ export default (props) => {
   let iconURL = `http://openweathermap.org/img/w/${props.weatherToday.icon}.png`
   return (
     <div className='results'>
-      <h1>{props.weatherToday.city}</h1>
+      <h2>{props.weatherToday.city}</h2>
+      <div className ="row">
       <h2>Today's Weather</h2>
-      <div className='today'>
-        {todayDay}
+
+      <div className="col-md-4">
+      </div>
+    
+      <div className='today col-md-4'>
+       <h4> {todayDay}</h4>
             <div className="temperature">
-            {Math.ceil(props.weatherToday.temp)}℃
+                <p>
+                {Math.ceil(props.weatherToday.temp)}℃
+                </p>
             </div>
 
             <div className="description">
-            {props.weatherToday.description}</div>
+                <p>
+                    {props.weatherToday.description}
+                </p>
+            </div>
 
-            <div className="icon"><img src={iconURL} /></div>
+            <div className="icon"><p><img src={iconURL} /></p> </div>
+           
 
             <div className="templow">
-            Min: {Math.ceil(props.weatherToday.low)}℃</div>
+                <p>
+                   <strong> Min:</strong> {Math.ceil(props.weatherToday.low)}℃
+                </p>
+            </div>
 
             <div className="temphigh">
-            Max: {Math.ceil(props.weatherToday.high)}℃</div>
+            <p>
+                <strong>Max:</strong> {Math.ceil(props.weatherToday.high)}℃
+            </p>
+            </div>
 
+            </div>
+     
+       <div className="col-md-4">
+            </div>
       </div>
+
+     <div className="future">
       <h2>3 Day Forecast</h2>
-      <div className="multiple">
-         {props.futureWeather.map((day) => {
+        <span>
+         {props.futureWeather.map((day, key) => {
          const a = new Date(day.date * 1000)
          const dayofWeek = days[a.getDay()]
          let dayIcon = `http://openweathermap.org/img/w/${day.icon}.png`
          return (
-            <div className="day">
-                <p>
-                    {dayofWeek}<br />
+           
+                <p className = "futurep">
+                    <h4>{dayofWeek}</h4><br />
                     {Math.ceil(day.temp)}℃<br />
                     {day.description} <br />
-                    <img src={dayIcon} />
+                    <img src={dayIcon} /><br/>
+                    <strong>Low:</strong> {Math.ceil(day.low)}℃<br />
+                    <strong>High:</strong> {Math.ceil(day.high)}℃
                 </p>
-                <p>
-                    {Math.ceil(day.low)}℃<br />
-                    {Math.ceil(day.high)}℃
-                </p>
-          </div>
+         
        )
     })}
+        </span>
       </div>
     </div>
   )
